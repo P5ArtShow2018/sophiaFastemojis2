489de16310584d5636bd95d531b176755bf81b3f
P5ArtShow2018 <38104389+P5ArtShow2018@users.noreply.github.com>
2018-04-23 10:01:35 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var Harold = function (){
+  var faceX = random(0,1000);
+  var faceY = random(0,1000);
+   noStroke();
+  //FILL
+  fill(252,249,70)
+  ellipse(faceX,faceY + 100,350)
+  fill(0);
+  //eyes
+  ellipse(faceX - 50,faceY,50)
+  ellipse(faceX + 60,faceY,50)
+  fill(255,0,0)
+  ellipse(faceX,faceY + 200,100)
+}
+
+
+function setup() {
+  createCanvas(1000,1000)
+  //frameRate(8);
+}
+
+function draw() {
+   background(255)
+ Harold();
+  Harold();
+   Harold();
+}
